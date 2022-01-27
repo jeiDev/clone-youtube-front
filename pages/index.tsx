baa03a95f2c2ab04fb79e467d8da1c5d92ec6dfa
jeiDev <joseanyeloserrano@gmail.com>
2022-01-26 23:19:37 -0400

    add checkbox for change mode theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
 import type { NextPage } from 'next'
 import Layout from '~/components/layout'
 import useSettingContext from "~/contexts/hooks/setting.context"
+
 const Home: NextPage = () => {
-  useSettingContext()
+  const settingContext = useSettingContext()
+
   return (
     <Layout>
         <h1 style={{textAlign: "center"}}>Youtube Clone</h1>
+        <div style={{display: "flex"}}>
+          <input type="checkbox" onChange={(e) => {
+            const value = e.target.checked;
+            console.log({value})
+            settingContext?.setTheme(value ? "dark" : "light")
+          }}/>
+          <span>Dark mode</span>
+        </div>
     </Layout>
   )
 }
